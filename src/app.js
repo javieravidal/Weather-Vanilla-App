cad821350600c08ab3c81329ccd9f9a04606a7bb
javieravidal <126521755+javieravidal@users.noreply.github.com>
2023-03-04 15:02:32 -0500

    Added matching icon

--- src/app.js
+++ src/app.js
@@ -15,24 +15,26 @@ function formatDate(timestamp) {
 
 
 function displayTemperature(response) {
-        console.log(response.data);
         let temperatureElement = document.querySelector("#temperature");
         let cityElement = document.querySelector("#city");
         let descriptionElement = document.querySelector("#weather-description");
         let humidityElement = document.querySelector("#humidity-percentage");
         let windElement = document.querySelector("#wind-speed");
         let dateElement = document.querySelector("#date");
+        let iconElement = document.querySelector("#icon");
+
         temperatureElement.innerHTML = Math.round(response.data.main.temp);
         cityElement.innerHTML = response.data.name;
         descriptionElement.innerHTML = response.data.weather[0].description;
         humidityElement.innerHTML = response.data.main.humidity;
         windElement.innerHTML = Math.round(response.data.wind.speed);
         dateElement.innerHTML = formatDate(response.data.dt*1000);
-
+        iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+        iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "9f9579090daa27d42e77fa3c3fec2fd7";
-let city = "Tokyo"
+let city = "Toronto"
 let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
